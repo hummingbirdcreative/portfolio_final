5162af6dfccee92e5a28f138f209f07697d7171b
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-10-18 19:23:11 -1000

    footer styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,17 @@
 function Footer(props) {
     return (
       <>
-          <div>
-            Copyright &copy; All Rights Reserved. 2022 Christie Codes.
+    <footer class="bg-white">
+      <div class="container mx-auto px-8">
+        <div class="w-full flex flex-col md:flex-row py-6">
+          <div class="flex-1 mb-6 text-black">
+            <a class="text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="/">
+              &copy; All Rights Reserved. 2022 Christie Codes Portfolio.
+            </a>
           </div>
+        </div>
+      </div>
+    </footer>
       </>
     );
   }
