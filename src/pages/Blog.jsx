54aec67c82dbbdb760ff9decb6cede766c451f57
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-10-19 10:08:54 -1000

    blog page styled

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>
-        <h1>Blog</h1>
+    <>
+    <div class="flex items-center justify-center h-screen bg-rose-500">
+  <div class="container">
+    <div class="bg-white rounded-lg shadow-lg p-5 md:p-20 mx-2">
+      <div class="text-center">
+        <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl mt-5">Blog Coming Soon
+        </h2>
+      </div>
+      
     </div>
-  )
+  </div>
+</div>
+
+    </>
+  );
 }
 
 export default Blog
